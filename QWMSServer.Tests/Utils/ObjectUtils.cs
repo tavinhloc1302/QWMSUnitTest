75065e870683947cdcc9d1f60a9041a2213cd0cd
Dinh Van Hoang <dvhoang1107@gmail.com>
2018-05-13 23:46:02 +0700

    Add helper modules.

---
+++ QWMSServer.Tests/Utils/ObjectUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace QWMSServer.Tests.Utils
+{
+    public static class ObjectUtils
+    {
+        public static T GetProperty<T>(Object obj, String name)
+        {
+            var prop = obj.GetType().GetProperty(name, typeof(T));
+            if (prop == null)
+                return default(T);
+
+            return (T)prop.GetValue(obj);
+        }
+
+        public static MethodInfo GetMethod(Object obj, String name)
+        {
+            return obj.GetType().GetMethod(name);
+        }
+    }
+}
