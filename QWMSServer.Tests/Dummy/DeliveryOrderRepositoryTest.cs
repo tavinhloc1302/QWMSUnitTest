40a9439142cafaee85e1ea4d7495f3c5363cba2f
Dinh Van Hoang <dvhoang1107@gmail.com>
2018-06-06 21:21:27 +0700

    Add Order dummy data.

---
+++ QWMSServer.Tests/Dummy/DeliveryOrderRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using QWMSServer.Data.Repository;
+using QWMSServer.Model.DatabaseModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QWMSServer.Tests.Dummy
+{
+    public class DeliveryOrderRepositoryTest : RepositoryBaseTest<DeliveryOrder>, IDeliveryOrderRepository
+    {
+        public override IList<DeliveryOrder> GetObjectList()
+        {
+            return new List<DeliveryOrder>() {
+            };
+        }
+    }
+}
---
+++ QWMSServer.Tests/Dummy/SaleOrderRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using QWMSServer.Data.Repository;
+using QWMSServer.Model.DatabaseModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QWMSServer.Tests.Dummy
+{
+    public class SaleOrderRepositoryTest : RepositoryBaseTest<SaleOrder>, ISaleOrderRepository
+    {
+        public override IList<SaleOrder> GetObjectList()
+        {
+            return new List<SaleOrder>() {
+            };
+        }
+    }
+}
